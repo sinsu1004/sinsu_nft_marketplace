ceb4a9785fa2636545afd2d0b1f9364f91e83a67
sinsu <sinsu19012@gmail.com>
2022-05-19 15:59:07 +0900

    style.css 변경전

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import {BrowserRouter as Router,Route} from 'react-router-dom';
 import React from 'react';
 
 
+//import "./assets/css/vendor/bootstrap.min.css";
 // import "./assets/css/plugins/feature.css"
 // import "./assets/css/plugins/jquery-ui.min.css"
-// import "./assets/css/vendor/bootstrap.min.css"
+
 // import "./assets/css/vendor/nice-select.css"
 // import "./assets/css/vendor/odometer.css"
 // import "./assets/css/vendor/slick-theme.css"
 // import "./assets/css/vendor/slick.css"  
+import './assets/css/style.css';
 
 
 
